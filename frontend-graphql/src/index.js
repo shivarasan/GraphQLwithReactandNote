0fd899227e81fa4704f5500f406393770587a62e
sivarasan <sivarasan.ponnuthurai@auxenta.com>
2019-12-03 11:02:40 +0530

    added create note api in foront end

--- frontend-graphql/src/index.js
+++ frontend-graphql/src/index.js
@@ -5,22 +5,27 @@ import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
 import { ApolloLink } from "apollo-link";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { onError } from 'apollo-link-error'
+import Notifications, {notify} from 'react-notify-toast';
 import "./index.css";
 import App from "./App";
-// import * as serviceWorker from "./serviceWorker";
-const httpLink = createHttpLink({ uri: "http://localhost:4300/graphql" });
-const link = ApolloLink.from([httpLink]);
+
+const errorLink = onError(({ graphQLErrors }) => {
+  if (graphQLErrors) graphQLErrors.map(({ message }) => notify.show(message, 'error'));
+});
+const httpLink = createHttpLink({ uri: 'http://localhost:4300/graphql' });
+const link = ApolloLink.from([
+  errorLink,
+  httpLink,
+]);
 const client = new ApolloClient({
   link,
   cache: new InMemoryCache()
 });
 ReactDOM.render(
   <ApolloProvider client={client}>
+    <Notifications />
     <App />
   </ApolloProvider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
